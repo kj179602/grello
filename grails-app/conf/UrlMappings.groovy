776a0cd19c2edc7944a3ae0dd4759b2989999d05
zwierze <tessst123abc@gmail.com>
2015-12-17 00:34:49 +0100

    Zmiana strony głównej

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -8,7 +8,8 @@ class UrlMappings {
             }
         }
 		
-        "/"(view:"/index")
+        //"/"(view:"/index")
+		"/"(controller:'user', action:"login")
         "500"(view:'/error')
 	}
 }
